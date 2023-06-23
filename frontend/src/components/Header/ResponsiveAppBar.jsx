96069b46419f5c782d24039a69ba0cd18f9ba756
van1llaface <arnoldaszeimis@gmail.com>
2023-06-23 10:50:43 +0300

    user_photo_change

--- frontend/src/components/Header/ResponsiveAppBar.jsx
+++ frontend/src/components/Header/ResponsiveAppBar.jsx
@@ -8,6 +8,7 @@ import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
@@ -202,7 +203,8 @@ function ResponsiveAppBar({ loggedIn, getLoggedIn, isAdmin }) {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+                <AccountCircleIcon fontSize="2rem" />
               </IconButton>
             </Tooltip>
             <Menu
